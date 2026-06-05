0b1109275cfc976ce19b677184f9c68f0035b9b3
S.AKSHARA <sakshara2007@gmail.com>
2026-06-05 09:48:04 +0530

    Update predict.py

--- DAY5/predict.py
+++ DAY5/predict.py
@@ -5,14 +5,16 @@ import pandas as pd
 with open("student_model.pkl", "rb") as file:
     model = pickle.load(file)
 
-# New Student Data
-new_student = pd.DataFrame({
-    "studytime": [3],
-    "absences": [2],
-    "failures": [0]
+# 3 Sample Students
+students = pd.DataFrame({
+    "studytime": [3, 1, 4],
+    "absences": [2, 15, 0],
+    "failures": [0, 2, 0]
 })
 
-# Prediction
-prediction = model.predict(new_student)
+# Predict Grades
+predictions = model.predict(students)
 
-print("Predicted Grade:", round(prediction[0], 2))
+# Display Results
+for i, pred in enumerate(predictions, start=1):
+    print(f"Student {i} Predicted Grade: {round(pred, 2)}")
